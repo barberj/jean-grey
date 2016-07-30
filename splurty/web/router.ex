74268a0fdaa63d783862890c80b8706d5dc5ef64
Justin Barber <barber.justin@gmail.com>
2016-07-29 20:45:28 -0400

    Add Json Routes

--- splurty/web/router.ex
+++ splurty/web/router.ex
@@ -21,7 +21,9 @@ defmodule Splurty.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", Splurty do
-  #   pipe_through :api
-  # end
+  scope "/api", Splurty, as: :api do
+     pipe_through :api
+
+     resources "/quotes", Api.QuoteController, except: [:new, :edit]
+  end
 end
---
+++ splurty/web/views/api/quote_view.ex
@@ -0,0 +1,17 @@
+defmodule Splurty.Api.QuoteView do
+  use Splurty.Web, :view
+
+  def render("index.json", %{quotes: quotes}) do
+    %{data: render_many(quotes, Splurty.Api.QuoteView, "quote.json")}
+  end
+
+  def render("show.json", %{quote: quote}) do
+    %{data: render_one(quote, Splurty.Api.QuoteView, "quote.json")}
+  end
+
+  def render("quote.json", %{quote: quote}) do
+    %{id: quote.id,
+      saying: quote.saying,
+      author: quote.author}
+  end
+end
